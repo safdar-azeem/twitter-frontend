4717abc0969cd5bf58efd89e48bf947a528a6bb1
safdar-azeem <safdarazeem00@gmail.com>
2021-07-01 09:55:00 +0000

    Add Notification.tsx

---
+++ src/pages/secure/Notification.tsx
@@ -0,0 +1,12 @@
+import NotificationsList from '../../components/notifications/NotificationsList'
+
+const Notification = () => {
+   return (
+      <div className="py-md-5 pt-1">
+         <h3 className="ms-md-4 fs-22">Notification</h3>
+         <NotificationsList />
+      </div>
+   )
+}
+
+export default Notification
