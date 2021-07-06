035da3c66385b5337dbf8c066ce2456f3087d822
safdar-azeem <safdarazeem00@gmail.com>
2021-07-06 18:00:00 +0000

    Add upload.service.ts

---
+++ src/store/services/upload.service.ts
@@ -0,0 +1,43 @@
+import Cookies from 'js-cookie'
+import { ApiResponse } from '../store'
+import { ITrend } from '../../types/trend.type'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const baseUrl = import.meta.env.VITE_API_URL as string
+
+type getTrendsResponse = {
+   message: string
+   trends: ITrend[]
+}
+
+const TrendsApi = createApi({
+   reducerPath: 'upload',
+   baseQuery: fetchBaseQuery({
+      baseUrl,
+      prepareHeaders: (headers, { getState }) => {
+         const token = Cookies.get('token')
+         if (token) {
+            headers.set('authorization', `Bearer ${token}`)
+         }
+         return headers
+      },
+   }),
+   tagTypes: ['Trends'],
+   endpoints: (builder) => ({
+      uplaodPhoto: builder.mutation<ApiResponse<'src', string>, any>({
+         query: (image) => {
+            const body = new FormData()
+            body.append('image', image)
+            return {
+               url: `/upload/photo`,
+               method: 'POST',
+               body,
+            }
+         },
+      }),
+   }),
+})
+
+export const { useUplaodPhotoMutation } = TrendsApi
+
+export default TrendsApi
